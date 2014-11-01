656631b32aeab2737daeb2cb6f9096b2187f267b
Emmanuel Rosales <emmanuelrs@gmail.com>
2014-11-01 15:49:31 -0600

    Cursos sing up

--- PetsWeb/Pets/process.php
+++ PetsWeb/Pets/process.php
@@ -2,12 +2,30 @@
 	$nombre = $_POST['NombreUsuario'];
 	$pass = $_POST['Contraseña'];
 
-	echo $nombre;
-	echo $pass;
-	if( $nombre !== ''){
-		if($pass == '123queso'){
-			header('refresh: 0; url=index.php');
-		};
+$conn = oci_connect('AD', 'ad', 'PETS','AL32UTF8');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+$query_procedimiento = oci_parse($conn, "BEGIN :cursor :=  consultas.valida_usuario(:nombre, :contrasena); END;");
+$cursor = oci_new_cursor($conn);
+oci_bind_by_name($query_procedimiento,':nombre', $nombre);
+oci_bind_by_name($query_procedimiento,':contrasena', $pass);
+oci_bind_by_name($query_procedimiento,':cursor', $cursor , -1, OCI_B_CURSOR);
+oci_execute($query_procedimiento);
+oci_execute($cursor, OCI_DEFAULT);
+oci_fetch_all($cursor, $array, null, null, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
 
-	};
+foreach ($array as $fila) {
+
+	$idpersona = $fila['ID_USUARIO'];
+}
+if($idpersona){
+	echo "sirve";
+}
+else{
+	header('refresh: 0; url=index.php');
+}
+
+
 ?>
