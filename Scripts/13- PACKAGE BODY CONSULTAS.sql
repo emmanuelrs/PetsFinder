f98757d4296ed7a45db4ad88af1046462ca3f5c2
Emmanuel Rosales <emmanuelrs@gmail.com>
2014-11-01 15:06:40 -0600

    actualizacion consultas

--- Scripts/13- PACKAGE BODY CONSULTAS.sql
+++ Scripts/13- PACKAGE BODY CONSULTAS.sql
@@ -1,16 +1,18 @@
 CREATE OR REPLACE PACKAGE BODY CONSULTAS AS
 
-FUNCTION VALIDA_USUARIO(USUARIO1 IN VARCHAR2, CONTRASENA IN VARCHAR)return number
- as id_persona number;
- BEGIN
-   select U.id_usuario INTO id_persona FROM USUARIO U
-   WHERE U.USER_NAME = USUARIO1 AND U.CONTRASEÑA = contrasena;
-  return id_persona;
- exception
-   when NO_DATA_FOUND then
-     id_persona := 0;
-     return id_persona;
-     
-  END VALIDA_USUARIO;
+FUNCTION VALIDA_USUARIO(USUARIO1 IN VARCHAR2, CONTRASENA IN VARCHAR2)
+  RETURN TYPES.ref_c
+  AS valida TYPES.ref_c;
+  BEGIN
+    OPEN valida FOR
+    select U.id_usuario 
+    FROM USUARIO U
+    WHERE U.USER_NAME = USUARIO1 AND U.CONTRASEÑA = contrasena;
+   return valida;
+  exception
+    when NO_DATA_FOUND then
+    return valida;
+
+   END VALIDA_USUARIO;
 
 END CONSULTAS;
