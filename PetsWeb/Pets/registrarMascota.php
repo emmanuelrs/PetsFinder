cc37d98e0e24e59c2c5f3571df5d9d98a022fa82
Emmanuel Rosales <emmanuelrs@gmail.com>
2014-11-03 15:36:47 -0600

    registrar mascota con la base

--- PetsWeb/Pets/registrarMascota.php
+++ PetsWeb/Pets/registrarMascota.php
@@ -77,7 +77,7 @@
 
 </script>
 <!-- End Analytics -->
-
+<div></div><div></div>
 <link rel="stylesheet" type="text/css" href="css/jquery-ui-1.7.2.custom.css" />
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
@@ -108,7 +108,7 @@ $(document).ready(function() {
    $("#datepicker").datepicker();
 });
 </script>
-
+</div>
 </head>
 <body>
 <nav id="menu">
--- PetsWeb/Pets/registrar_mascota.php
+++ PetsWeb/Pets/registrar_mascota.php
@@ -19,24 +19,44 @@ if (!$conn) {
 	$var13 = $_POST['recompenza'];
 	$var14 = $_POST['descripcion'];
     $var15 = $_POST['datepicker'];
+    $fecha = (string)$var15
     $var16 = 1;
     $target_path = basename($_FILES['uploadedfile']['name']); 
 	$imagen = (string)$target_path;
 
-echo $var1;
-echo $var2;
-echo $var3;
-echo $var5;
-echo $var6;
-echo $var7;
-echo $var8;
-echo $var9;
-echo $var10;
-echo $var11;
-echo $var12;
-echo $var13;
-echo $var14;
-echo $var15;
-echo $imagen;
+	if($var7 == "Encontrada"){
+        
+        $stid = ociparse($conn, "BEGIN  insertions.set_mascota_encontrada(:p1,:p2,:p3,:p4,:p5,:p6,:p7,:p8,:p9,:p10,:p11,:p12,:p13,:p14,:p15,:p16,:p17); END;");
+        
+    }else if($var7 == "Perdida"){
+        
+        $stid = ociparse($conn, "BEGIN insertions.set_mascota_perdida(:p1,:p2,:p3,:p4,:p5,:p6,:p7,:p8,:p9,:p10,:p11,:p12,:p13,:p14,:p15,:p16,:p17); END;");
+      
+    }
+
+
+    oci_bind_by_name($stid, ':p1', $var1);
+    oci_bind_by_name($stid, ':p2', $var2);
+    oci_bind_by_name($stid, ':p3', $var3);
+    oci_bind_by_name($stid, ':p4', $imagen);
+    oci_bind_by_name($stid, ':p5', $var5);
+    oci_bind_by_name($stid, ':p6', $var6);
+    oci_bind_by_name($stid, ':p7', $var7);
+    oci_bind_by_name($stid, ':p8', $var8);
+    oci_bind_by_name($stid, ':p9', $var9);
+    oci_bind_by_name($stid, ':p10', $var10);
+    oci_bind_by_name($stid, ':p11', $var11);
+    oci_bind_by_name($stid, ':p12', $var12);
+    oci_bind_by_name($stid, ':p13', $var13);
+    oci_bind_by_name($stid, ':p14', $var14);
+    oci_bind_by_name($stid, ':p15', $fecha);
+    oci_bind_by_name($stid, ':p16', $var16);
+    
+	oci_execute($stid);
+	oci_free_statement($stid);
+    oci_close($conn);
+
+
+
 
 ?> 
