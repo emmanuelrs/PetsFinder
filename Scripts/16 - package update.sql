7a6f9501f1aeffffeed2d773271b9977e9186fbd
lserrano467 <luis_ser_san@hotmail.com>
2014-11-03 16:09:36 -0600

    cambio de id_user a  username

--- Scripts/16 - package update.sql
+++ Scripts/16 - package update.sql
@@ -6,7 +6,7 @@ PROCEDURE UPDATE_MASCOTA_ENC(id_masc in number, nombre_masc in varchar2, raza_ma
 PROCEDURE UPDATE_MASCOTA_PER(id_masc in number, nombre_masc in varchar2, raza_masc in varchar2, tamano in varchar2, 
   chip in varchar2, color in varchar2, estado in varchar2, recompenza in varchar2, descrip in varchar2);  
 
-PROCEDURE UPDATE_USUARIO(id_user in number, nom in varchar2, ape1 in varchar2, ape2 in varchar2, username in varchar2,
+PROCEDURE UPDATE_USUARIO(nom in varchar2, ape1 in varchar2, ape2 in varchar2, username in varchar2,
    contra in varchar2);
    
 PROCEDURE UPDATE_EMAIL(id_emailN in number, tipo in number, nuevo_email in varchar2);
