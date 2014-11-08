ebdbeba35ac3b714bb63d6506219788f85c2b9f6
Emmanuel Rosales <emmanuelrs@gmail.com>
2014-11-08 12:37:15 -0600

    Busquedas Provincia
    Falta la divi'on para pa imagen

--- PetsWeb/Pets/bus-provincia.php
+++ PetsWeb/Pets/bus-provincia.php
@@ -11,7 +11,7 @@ $division = '';
 
  if($tipo == "Encontrada"){
         
-    $query_procedimiento = oci_parse($conn, "BEGIN :cursor := busquedas.busquedapeliculasid(:busqueda); END;");  
+    $query_procedimiento = oci_parse($conn, "BEGIN :cursor := busquedas.busquedaprovincia_enc(:busq); END;");  
 	$cursor = oci_new_cursor($conn);
 	oci_bind_by_name($query_procedimiento,':busq', $provincia);
 	oci_bind_by_name($query_procedimiento,':cursor', $cursor , -1, OCI_B_CURSOR);
@@ -19,13 +19,13 @@ $division = '';
 	oci_execute($cursor, OCI_DEFAULT);
 	oci_fetch_all($cursor, $array, null, null, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
 	foreach ($array as $fila) {
-		$division =  $division."<br></br>Nombre de la mascota: ".$fila['NOMBRE_ENC']."<br></br> Estado: "
-		.$fila['ESTADO_ENC']."<br></br> País: ".$fila['PAIS']."<br></br> Provincia: ".$fila['PROVINCIA'];}
+		$division =  $division."<br></br>Nombre de la mascota: ".$fila['NOMBRE_ENC']."<br></br> País: ".$fila['PAIS']."<br></br> Canton: ".$fila['CANTON']
+		."<br></br> Distrito: ".$fila['DISTRITO']. "<br></br> Dirección: ".$fila['DIRECCION_EXACTA']."<br></br> Fecha de perdida: ".$fila['FECHA_INGRESO_ENC'];}
 echo $division;
         
 }else if($tipo == "Perdida"){
         
-    $query_procedimiento = oci_parse($conn, "BEGIN :cursor := busquedas.busquedapeliculastitulo(:busqueda); END;");
+    $query_procedimiento = oci_parse($conn, "BEGIN :cursor := busquedas.busquedaprovincia_per(:busq); END;");
     $cursor = oci_new_cursor($conn);
 	oci_bind_by_name($query_procedimiento,':busq', $provincia);
 	oci_bind_by_name($query_procedimiento,':cursor', $cursor , -1, OCI_B_CURSOR);
@@ -33,8 +33,8 @@ echo $division;
 	oci_execute($cursor, OCI_DEFAULT);
 	oci_fetch_all($cursor, $array, null, null, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
 	foreach ($array as $fila) {
-		$division =  $division."<br></br>Nombre de la mascota: ".$fila['NOMBRE_PER']."<br></br> Estado: "
-		.$fila['ESTADO_PER']."<br></br> País: ".$fila['PAIS']."<br></br> Provincia: ".$fila['PROVINCIA'];}
+		$division =  $division."<br></br>Nombre de la mascota: ".$fila['NOMBRE_PER']."<br></br> País: ".$fila['PAIS']."<br></br> Canton: ".$fila['CANTON']
+		."<br></br> Distrito: ".$fila['DISTRITO']. "<br></br> Dirección: ".$fila['DIRECCION_EXACTA']."<br></br> Fecha de perdida: ".$fila['FECHA_INGRESO_PER'];}
 echo $division;
              
 }
