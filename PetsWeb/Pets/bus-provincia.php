2e78a8032438ab428fb82d234db8e0aea8676ec6
Emmanuel Rosales <emmanuelrs@gmail.com>
2014-11-08 11:49:21 -0600

    busqueda con ajax

--- PetsWeb/Pets/bus-provincia.php
+++ PetsWeb/Pets/bus-provincia.php
@@ -4,9 +4,21 @@ if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-	
-$pro = $_POST['busqueda'];
-echo $pro;
+$provincia = $_POST['busqueda'];	
+$division = '';
+$query_procedimiento = oci_parse($conn, "BEGIN :cursor := busquedas.busquedaprovincia(:busq); END;");
 
+$cursor = oci_new_cursor($conn);
+oci_bind_by_name($query_procedimiento,':busq', $provincia);
+oci_bind_by_name($query_procedimiento,':cursor', $cursor , -1, OCI_B_CURSOR);
+oci_execute($query_procedimiento);
+oci_execute($cursor, OCI_DEFAULT);
+oci_fetch_all($cursor, $array, null, null, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
+foreach ($array as $fila) {
+	$division =  $division."<br></br>Nombre de la mascota: ".$fila['NOMBRE_ENC']."<br></br> Estado: "
+	.$fila['ESTADO_ENC']."<br></br> País: ".$fila['PAIS']."<br></br> Provincia: ".$fila['PROVINCIA'];}
+
+
+echo $division;
 
 ?>
