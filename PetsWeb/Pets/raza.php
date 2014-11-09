2fe083783f3506142723ea3e757099f5c62aa4b1
Emmanuel Rosales <emmanuelrs@gmail.com>
2014-11-08 19:27:31 -0600

    Busquedas por Raza

--- PetsWeb/Pets/raza.php
+++ PetsWeb/Pets/raza.php
@@ -57,92 +57,42 @@
 <link rel="apple-touch-icon" sizes="114x114" href="#">
 <link rel="apple-touch-icon" sizes="72x72" href="#">
 <link rel="apple-touch-icon" sizes="144x144" href="#">
-
-<!-- Modernizr -->
-<script src="_include/js/modernizr.js"></script>
-
-
-<!-- Analytics -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'Insert Your Code']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-<!-- End Analytics -->
-<div></div><div></div>
-<link rel="stylesheet" type="text/css" href="css/jquery-ui-1.7.2.custom.css" />
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js"></script>
-<script type="text/javascript">
-jQuery(function($){
-$.datepicker.regional['es'] = {
-closeText: 'Cerrar',
-prevText: '&#x3c;Ant',
-nextText: 'Sig&#x3e;',
-currentText: 'Hoy',
-monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
-'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
-'Jul','Ago','Sep','Oct','Nov','Dic'],
-dayNames: ['Domingo','Lunes','Martes','Mi&eacute;rcoles','Jueves','Viernes','S&aacute;bado'],
-dayNamesShort: ['Dom','Lun','Mar','Mi&eacute;','Juv','Vie','S&aacute;b'],
-dayNamesMin: [' D ',' L ',' K ',' M ',' J ',' V ',' S '],
-weekHeader: 'Sm',
-dateFormat: 'dd/mm/yy',
-firstDay: 1,
-isRTL: false,
-showMonthAfterYear: false,
-yearSuffix: ''};
-$.datepicker.setDefaults($.datepicker.regional['es']);
-});    
- 
-$(document).ready(function() {
-   $("#datepicker").datepicker();
-});
-</script>
 </div>
 </head>
 <body>
 <header>
- <div class="sticky-nav">
-    <div class="span">
-    <img src="_include/img/work/logo.png" width="180" height="90">
+<div class="sticky-nav">
+<div class="span">
+<img src="_include/img/work/logo.png" width="180" height="90">
 </div>
-        <a id="mobile-nav" class="menu-nav" href="#menu-nav"></a>
-        <nav id="menu">
-            <ul id="menu-nav">
-                <li class="current"><a href="index activo.php" class = "external">Inicio</a></li
-          ></ul>
-      </nav>
-        
-    </div>
+<a id="mobile-nav" class="menu-nav" href="#menu-nav"></a>
+<nav id="menu">
+<ul id="menu-nav">
+<li class="current"><a href="index activo.php" class = "external">Inicio</a></li></ul>
+</nav>
+</div>
 </header>
- <div class="span1">
-          <form id="contact-form" class="contact-form" action="bus-raza.php" method="POST">
-
-          <br></br>
-               <select name="Raza">
-                <option selected value="0"> Seleccione la raza</option>
-              <optgroup label="Perros">
-              <option value="Salchicha">Salchicha</option>
-              <option value="Gran Danes">Gran Danés</option>
-                <option value="Raza Unica">Raza Única</option>
-                <optgroup label="Gatos">
-              <option value="Angora">Angora</option>
-            <option value="Raza Unica">Raza Única</option>
-          </optgroup>
-        </select>
-                <input type="submit" value="Enviar" class="boton">  
-             
-                </form>  
-  </div>              
+<div class="span1">
+<form id="contact-form" class="contact-form" action="bus-raza.php" method="POST">
+<br></br>
+<select name="Raza">
+<option selected value="0"> Seleccione la raza</option>
+<optgroup label="Perros">
+<option value="Salchicha">Salchicha</option>
+<option value="Gran Danes">Gran Danés</option>
+<option value="Raza Unica">Raza Única</option>
+<optgroup label="Gatos">
+<option value="Angora">Angora</option>
+<option value="Raza Unica">Raza Única</option>
+</optgroup>
+</select>
+<select id = "tipo" name = "tipoBusqueda">
+<option selected value = "0">Estado</option>
+<option value = "Encontrada">Encontrada</option>
+<option value = "Perdida">Perdida</option>
+</select>
+<br><input type="submit" value="Buscar" class="boton"></div> </br> 
+</form> 
+</div>              
 </body>
 </html>
