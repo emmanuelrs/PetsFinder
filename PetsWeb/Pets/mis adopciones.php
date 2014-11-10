39458aa8972b2643412b558c2520d1f30a7c8456
lserrano467 <luis_ser_san@hotmail.com>
2014-11-09 21:40:56 -0600

    mis adopciones user normal

--- PetsWeb/Pets/mis adopciones.php
+++ PetsWeb/Pets/mis adopciones.php
@@ -1,3 +1,8 @@
+<?php
+// Start the session
+session_start();
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
@@ -113,3 +118,30 @@ function ver(num) {
 
 </body>
 </html>
+
+<?php
+
+$conn = oci_connect('AD', 'ad', 'PETS','AL32UTF8');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+$idPer = $_SESSION['IDU'];
+$division = '';
+
+$query_procedimiento = oci_parse($conn, "BEGIN :cursor :=consultas.misadop(:busq); END;");  
+$cursor = oci_new_cursor($conn);
+oci_bind_by_name($query_procedimiento,':busq', $idPer);
+oci_bind_by_name($query_procedimiento,':cursor', $cursor , -1, OCI_B_CURSOR);
+oci_execute($query_procedimiento);
+oci_execute($cursor, OCI_DEFAULT);
+oci_fetch_all($cursor, $array, null, null, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
+foreach ($array as $fila) {
+  $division = $division .' <div id="general"><div id="foticas"><br></br><img src = "img/'. $fila['NOMBRE_IMG'].'"'.'width=300px height = 350px></div>
+  <br></br>Nombre: '.$fila['NOMBRE_ADOP'].'<br></br> Chip de Identificación: '.$fila['CHIP_IDENTIFICACION_ADOP'].'<br></br> Raza: '.$fila['DESCRIPCION_RAZA'];}
+
+echo $division;
+
+
+?>
