018a520f46fd80a390bb0cfc771503a223c2ac6e
lserrano467 <luis_ser_san@hotmail.com>
2014-11-10 17:55:59 -0600

    O.o

--- Scripts/22 - PACKAGE BODY DONACIONES.sql
+++ Scripts/22 - PACKAGE BODY DONACIONES.sql
@@ -55,11 +55,10 @@ FUNCTION CONSULTA_DONACION(username in varchar2)
 FUNCTION CONSULTA_org
         return TYPES.ref_c
         AS org types.ref_c;
-        begin       
+        begin
          OPEN org FOR
-         SELECT o.nombre_org, u.nombre, u.apellido1, d.cantidad
-         FROM organizacion_casa_cuna o, usuario u, donaciones d
-         WHERE d.id_donacion = o.id_organizacion and d.donador = id_user;      
+         SELECT o.nombre_org
+         FROM organizacion_casa_cuna o;
          return org;
     END CONSULTA_org;
 
