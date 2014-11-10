2b1b2055946dcc1c66634796c3c9f9d58d7db83d
lserrano467 <luis_ser_san@hotmail.com>
2014-11-09 19:19:58 -0600

    cursor casa cuna + direccion

--- Scripts/15- PACKAGES BODY BUSQUEDAS.sql
+++ Scripts/15- PACKAGES BODY BUSQUEDAS.sql
@@ -174,10 +174,10 @@ FUNCTION busquedaRaza_Adop(pRaza IN VARCHAR2)
   AS busqRaza TYPES.ref_c;
   BEGIN
     OPEN busqRaza FOR
-    SELECT u.nombre, u.apellido1, u.calificacion, e.valor_email, t.telefono
-    FROM usuario u, email e, telefono t, casa_cuna c, raza r
-    WHERE r.descripcion_raza = pRaza and c.id_raza = r.id_raza and c.estado = 'Autorizado' and c.id_persona = u.id_usuario and e.id_email = u.id_usuario and e.tipo_email = u.tipo 
-          and t.id_tel = u.id_usuario and t.tipo_tel = u.tipo;
+    SELECT u.nombre, u.apellido1, u.calificacion, e.valor_email, t.telefono , d.provincia, d.canton, d.direccion_exacta
+    FROM usuario u, email e, telefono t, casa_cuna c, raza r, direccion d
+    WHERE r.descripcion_raza = 'Chiguagua' and c.id_raza = r.id_raza and c.estado = 'Autorizado' and c.id_persona = u.id_usuario and e.id_email = u.id_usuario and e.tipo_email = u.tipo 
+          and t.id_tel = u.id_usuario and t.tipo_tel = u.tipo and u.id_usuario = d.id_direccion and u.tipo = d.tipo_direc;
     RETURN busqRaza;
  END busquedaRaza_Adop;
 
