1d8cdb41761e9ef085e4ad65217ad4b05cecd7a8
Emmanuel Rosales <emmanuelrs@gmail.com>
2014-11-10 12:14:07 -0600

    actualizacion

--- PetsWeb/Pets/process.php
+++ PetsWeb/Pets/process.php
@@ -9,12 +9,13 @@ session_start();
 	$pass = $_POST['Contraseña'];
 	$_SESSION["NU"] = $nombre;
     
-
 $conn = oci_connect('AD', 'ad', 'PETS','AL32UTF8');
 if (!$conn) {
     $e = oci_error();
     trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
+
+
 $query_procedimiento = oci_parse($conn, "BEGIN :cursor :=  consultas.valida_usuario(:nombre, :contrasena); END;");
 $cursor = oci_new_cursor($conn);
 oci_bind_by_name($query_procedimiento,':nombre', $nombre);
