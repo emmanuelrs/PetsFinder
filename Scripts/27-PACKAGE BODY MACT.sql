2ce93a45479555bd7d026025ee41127b81f41676
lserrano467 <luis_ser_san@hotmail.com>
2014-11-10 15:23:58 -0600

    magia negra

--- Scripts/27-PACKAGE BODY MACT.sql
+++ Scripts/27-PACKAGE BODY MACT.sql
@@ -12,14 +12,15 @@ FUNCTION MATCH2
     RETURN match1;
 
  END MATCH2;
- 
+
  PROCEDURE BUSCA_MATCH AS
    id_m number;
    nom varchar2(30);
    ape varchar2(30);
    email varchar2(100);
+   status varchar2(30);
    begin
-     select mp.id_mascota_per, u.nombre, u.apellido1, e.valor_email into id_m, nom, ape, email
+    select mp.id_mascota_per, u.nombre, u.apellido1, e.valor_email into id_m, nom, ape, email
     from match3 m3, mascota_perdida mp, usuario u, email e
     where m3.id_mascota_per = mp.id_mascota_per and u.id_usuario = mp.usuario_registra_per and u.id_usuario = e.id_email and u.tipo = e.tipo_email;
     
@@ -28,6 +29,25 @@ FUNCTION MATCH2
     commit;
  end BUSCA_MATCH;
  
-END MATC;
+PROCEDURE RECUPERAR(ID_MASC_ENC IN VARCHAR2) AS
+   id_m number;
+   begin
+     id_m := TO_NUMBER(RIGHT => ID_MASC_ENC);
+     update mascota_encontrada m
+     set m.estado_enc = 'Recuperada'
+     where m.id_mascota_enc = id_m;
+     commit;
+ END RECUPERAR;
  
+PROCEDURE NO_COINCIDE(ID_MASC_ENC IN VARCHAR2) AS
+   id_m number;
+   begin
+     id_m := TO_NUMBER(RIGHT => ID_MASC_ENC);
+     update mascota_encontrada m
+     set m.estado_enc = 'Inactiva'
+     where m.id_mascota_enc = id_m;
+     commit;
+ END NO_COINCIDE;
+ 
+END MATC;
  
