4f755f70414cdde3d9eb7a019d2d8dbeb00a4f9f
Emmanuel Rosales <emmanuelrs@gmail.com>
2014-11-09 19:32:48 -0600

    Actalizacion

--- PetsWeb/Pets/conCCuna.php
+++ PetsWeb/Pets/conCCuna.php
@@ -19,7 +19,8 @@ oci_fetch_all($cursor, $array, null, null, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC
 foreach ($array as $fila) {
 	$division = $division .' <div id="general"> <div id="foticas"><img src ="img/casa.png"'.'width=300px height = 350px></div>
 	<br></br>Nombre: '.$fila['NOMBRE'].'<br></br> Apellido: '.$fila['APELLIDO1'].'<br></br> Calificación: '.$fila['CALIFICACION'].'<br></br> Email: '
-	.$fila['VALOR_EMAIL']."<br></br> Teléfono: ".$fila['TELEFONO'];}
+	.$fila['VALOR_EMAIL']."<br></br> Teléfono: ".$fila['TELEFONO']."<br></br> Provincia: ".$fila['PROVINCIA']."<br></br> Cantón: ".$fila['CANTON']
+	."<br></br> Dirreción: ".$fila['DIRECCION_EXACTA']."<br></br> Requiere Alimento: ".$fila['REQUIERE_ALIMENTO'];}
 
 echo $division;
 
