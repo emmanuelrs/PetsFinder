dd9e21d24330d3ea77cbcc50fe7304b4bbcc8d0c
Emmanuel Rosales <emmanuelrs@gmail.com>
2014-11-10 11:12:04 -0600

    perdas\

--- PetsWeb/Pets/mismascoperdidas.php
+++ PetsWeb/Pets/mismascoperdidas.php
@@ -88,7 +88,31 @@ session_start();
 </p>
 <button  type="submit">Ver Match</button>
 </form>
-
-
 <div id = "resp"></div>
 </html>
+
+<?php
+<?php
+$conn = oci_connect('AD', 'ad', 'PETS','AL32UTF8');
+if (!$conn) {
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+$idUsuario = $_SESSION['IDU'];
+$division = '';    
+
+$query_procedimiento = oci_parse($conn, "BEGIN :cursor := busquedas.busquedaprovincia_per(:busq); END;");
+$cursor = oci_new_cursor($conn);
+oci_bind_by_name($query_procedimiento,':busq', $provincia);
+oci_bind_by_name($query_procedimiento,':cursor', $cursor , -1, OCI_B_CURSOR);
+oci_execute($query_procedimiento);
+oci_execute($cursor, OCI_DEFAULT);
+oci_fetch_all($cursor, $array, null, null, OCI_FETCHSTATEMENT_BY_ROW + OCI_ASSOC);
+
+foreach ($array as $fila) {
+    $division = $division .' <div id="general"> <div id="foticas"><br></br><br></br> <img src = "img/'. $fila['NOMBRE_IMG'].'"'.'width=300px height = 350px></div>
+    <br></br>Nombre de la mascota: '.$fila['NOMBRE_PER'].'<br></br> País: '.$fila['PAIS'].'<br></br> Canton: '.$fila['CANTON'].'<br></br> Distrito: '
+    .$fila['DISTRITO']."<br></br> Dirección: ".$fila['DIRECCION_EXACTA']."<br></br> Fecha de perdida: ".$fila['FECHA_INGRESO_PER'];}
+echo $division;
+
+?>
